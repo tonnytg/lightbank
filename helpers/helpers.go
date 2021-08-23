8d161084413e636840cf070dafcd941f831457a0
Antonio Thomacelli <tonnytg@gmail.com>
2021-08-22 23:12:02 -0300

    feat: add helpers

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,1 +1,19 @@
 package helpers
+
+import (
+	"golang.org/x/crypto/bcrypt"
+	_ "golang.org/x/crypto/bcrypt"
+)
+
+func HandleErr(err error) {
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func HashAndSalt(pass []byte) string {
+	hashed, err := bcrypt.GenerateFromPassword(pass, bcrypt.MinCost)
+	HandleErr(err)
+
+	return string(hashed)
+}
