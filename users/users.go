ff493201a430e11552cb2091453c42e53c17ed8b
Antonio Thomacelli <tonnytg@gmail.com>
2021-08-23 22:20:46 -0300

    feat: add user and jwt

--- users/users.go
+++ users/users.go
@@ -1,1 +1,53 @@
 package users
+
+import (
+	"github.com/tonnytg/lightbank/helpers"
+	"github.com/tonnytg/lightbank/interfaces"
+	"golang.org/x/crypto/bcrypt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func Login(username string, pass string) map[string]interface{} {
+	db := helpers.ConnectDB()
+	user := &interfaces.User{}
+	if err := db.Where("username = ? ", username).First(&user).Error; err != nil {
+		return map[string]interface{}{"message": "User not found"}
+	}
+
+	// Verify GenerateFromPassword
+	passErr := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(pass))
+
+	if passErr == bcrypt.ErrMismatchedHashAndPassword && passErr != nil {
+		return map[string]interface{}{"message": "Wrong password"}
+	}
+
+	// Find account for the username
+	var accounts []interfaces.ResponseAccount
+	db.Table("account").Select("id, name, balance").Where("user_id = ?", user.ID).Scan(&accounts)
+
+	// Setup response
+	responseUser := &interfaces.ResponseUser{
+		ID:       user.ID,
+		Username: user.Username,
+		Email:    user.Email,
+		Accounts: accounts,
+	}
+
+	// Sign token
+	tokenContent := jwt.MapClaims{
+		"user_id": user.ID,
+		"expiry":  time.Now().Add(time.Minute * 60).Unix(),
+	}
+	jwtToken := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tokenContent)
+	token, err := jwtToken.SignedString([]byte("TokenPassword"))
+	helpers.HandleErr(err)
+
+	// Prepare response
+	var response = map[string]interface{}{"message": "all is fine"}
+	response["jwt"] = token
+	response["data"] = responseUser
+
+	return response
+}
