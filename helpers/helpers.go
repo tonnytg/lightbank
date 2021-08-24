e9675f05e98751a0f9e61d3c1c1ac7e17447b5fb
Antonio Thomacelli <tonnytg@gmail.com>
2021-08-23 21:07:40 -0300

    feat: centralize db connect in helpers

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -3,6 +3,8 @@ package helpers
 import (
 	"golang.org/x/crypto/bcrypt"
 	_ "golang.org/x/crypto/bcrypt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func HandleErr(err error) {
@@ -16,4 +18,11 @@ func HashAndSalt(pass []byte) string {
 	HandleErr(err)
 
 	return string(hashed)
+}
+
+func ConnectDB() *gorm.DB {
+	dsn := "host=127.0.0.1 user=postgres password=postgres dbname=lightbank port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	HandleErr(err)
+	return db
 }
