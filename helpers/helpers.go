7e539309f75abca0ea42acaf4dce02293291e173
Antonio Thomacelli <tonnytg@gmail.com>
2021-08-24 22:36:02 -0300

    feat: validation user and email

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,10 +1,12 @@
 package helpers
 
 import (
+	"github.com/tonnytg/lightbank/interfaces"
 	"golang.org/x/crypto/bcrypt"
 	_ "golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"regexp"
 )
 
 func HandleErr(err error) {
@@ -25,4 +27,27 @@ func ConnectDB() *gorm.DB {
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	HandleErr(err)
 	return db
+}
+
+func Validation(values []interfaces.Validation) bool {
+	username := regexp.MustCompile(`^([A-Za-z0-9]{5,})+$`)
+	email := regexp.MustCompile(`^[A-Za-z0-9]+[@]+[A-Za-z0-9]+[.]+[A-Za-z]+$`)
+
+	for i := 0; i < len(values); i++ {
+		switch values[i].Valid {
+		case "username":
+			if !username.MatchString(values[i].Value) {
+				return false
+			}
+		case "email":
+			if !email.MatchString(values[i].Value) {
+				return false
+			}
+		case "password":
+			if len(values[i].Value) < 5 {
+				return false
+			}
+		}
+	}
+	return true
 }
