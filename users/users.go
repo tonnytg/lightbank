d2a14403920dd63d1e1ba8b8d67405340656b1f1
Antonio Thomacelli <tonnytg@gmail.com>
2021-08-24 22:37:24 -0300

    feat: add login and register validation

--- users/users.go
+++ users/users.go
@@ -9,32 +9,7 @@ import (
 	"github.com/golang-jwt/jwt/v4"
 )
 
-func Login(username string, pass string) map[string]interface{} {
-	db := helpers.ConnectDB()
-	user := &interfaces.User{}
-	if err := db.Where("username = ? ", username).First(&user).Error; err != nil {
-		return map[string]interface{}{"message": "User not found"}
-	}
-
-	// Verify GenerateFromPassword
-	passErr := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(pass))
-
-	if passErr == bcrypt.ErrMismatchedHashAndPassword && passErr != nil {
-		return map[string]interface{}{"message": "Wrong password"}
-	}
-
-	// Find account for the username
-	var accounts []interfaces.ResponseAccount
-	db.Table("account").Select("id, name, balance").Where("user_id = ?", user.ID).Scan(&accounts)
-
-	// Setup response
-	responseUser := &interfaces.ResponseUser{
-		ID:       user.ID,
-		Username: user.Username,
-		Email:    user.Email,
-		Accounts: accounts,
-	}
-
+func prepareToken(user *interfaces.User) string {
 	// Sign token
 	tokenContent := jwt.MapClaims{
 		"user_id": user.ID,
@@ -44,10 +19,85 @@ func Login(username string, pass string) map[string]interface{} {
 	token, err := jwtToken.SignedString([]byte("TokenPassword"))
 	helpers.HandleErr(err)
 
+	return token
+}
+
+func prepareResponse(user *interfaces.User, accounts []interfaces.ResponseAccount) map[string]interface{} {
+
+	// Setup response
+	responseUser := &interfaces.ResponseUser{
+		ID:       user.ID,
+		Username: user.Username,
+		Email:    user.Email,
+		Accounts: accounts,
+	}
+
 	// Prepare response
+	var token = prepareToken(user)
 	var response = map[string]interface{}{"message": "all is fine"}
 	response["jwt"] = token
 	response["data"] = responseUser
 
 	return response
+}
+
+func Login(username string, pass string) map[string]interface{} {
+
+	valid := helpers.Validation(
+		[]interfaces.Validation{
+			{Value: username, Valid: "username"},
+			{Value: pass, Valid: "password"},
+		})
+	if valid {
+		db := helpers.ConnectDB()
+		user := &interfaces.User{}
+		if err := db.Where("username = ? ", username).First(&user).Error; err != nil {
+			return map[string]interface{}{"message": "User not found"}
+		}
+
+		// Verify GenerateFromPassword
+		passErr := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(pass))
+
+		if passErr == bcrypt.ErrMismatchedHashAndPassword && passErr != nil {
+			return map[string]interface{}{"message": "Wrong password"}
+		}
+
+		// Find account for the username
+		var accounts []interfaces.ResponseAccount
+		db.Table("account").Select("id, name, balance").Where("user_id = ?", user.ID).Scan(&accounts)
+
+		var response = prepareResponse(user, accounts)
+
+		return response
+	} else {
+		return map[string]interface{}{"message": "not valid volumes"}
+	}
+}
+
+func Register(username string, email string, pass string) map[string]interface{} {
+	// Add validation to registration
+	valid := helpers.Validation(
+		[]interfaces.Validation{
+			{Value: username, Valid: "username"},
+			{Value: email, Valid: "email"},
+			{Value: pass, Valid: "password"},
+		})
+	if valid {
+		db := helpers.ConnectDB()
+		generatedPassword := helpers.HashAndSalt([]byte(pass))
+		user := &interfaces.User{Username: username, Email: email, Password: generatedPassword}
+		db.Create(&user)
+
+		account := &interfaces.Account{Type: "Daily Account", Name: string(username + "'s" + " account"), Balance: 0, UserID: user.ID}
+		db.Create(&account)
+
+		accounts := []interfaces.ResponseAccount{}
+		respAccount := interfaces.ResponseAccount{ID: account.ID, Name: account.Name, Balance: int(account.Balance)}
+		accounts = append(accounts, respAccount)
+		var response = prepareResponse(user, accounts)
+
+		return response
+	} else {
+		return map[string]interface{}{"message": "not valid values"}
+	}
 }
