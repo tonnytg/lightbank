925d4e33c2f2b931e9808cb667ef21d88cdecb31
Antonio Thomacelli <tonnytg@gmail.com>
2021-08-25 00:00:31 -0300

    feat: add update balance account

--- useraccounts/useraccounts.go
+++ useraccounts/useraccounts.go
@@ -1,1 +1,11 @@
 package useraccounts
+
+import (
+	"github.com/tonnytg/lightbank/helpers"
+	"github.com/tonnytg/lightbank/interfaces"
+)
+
+func updateAccount(id uint, amount int) {
+	db := helpers.ConnectDB()
+	db.Model(&interfaces.Account{}).Where("id = ?", id).Update("balance", amount)
+}
