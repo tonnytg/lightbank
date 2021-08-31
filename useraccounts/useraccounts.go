63fed9586b69638f28c8ddd5fe82eef8c164ca67
Antonio Thomacelli <tonnytg@gmail.com>
2021-08-30 23:42:41 -0300

    feat: transaction check

--- useraccounts/useraccounts.go
+++ useraccounts/useraccounts.go
@@ -1,6 +1,8 @@
 package useraccounts
 
 import (
+	"fmt"
+
 	"github.com/tonnytg/lightbank/helpers"
 	"github.com/tonnytg/lightbank/interfaces"
 )
@@ -8,4 +10,24 @@ import (
 func updateAccount(id uint, amount int) {
 	db := helpers.ConnectDB()
 	db.Model(&interfaces.Account{}).Where("id = ?", id).Update("balance", amount)
+}
+
+func getAccount(id uint) *interfaces.Account {
+	db := helpers.ConnectDB()
+	account := &interfaces.Account{}
+	if db.Where("id = ? ", id).First(&account).Error != nil {
+		return nil
+	}
+
+	return account
+}
+
+func Transaction(userId uint, from uint, to uint, amount int, jwt string) map[string]interface{} {
+	userIdString := fmt.Sprint(userId)
+	isValid := helpers.ValidateToken(userIdString, jwt)
+	if isValid {
+
+	} else {
+		return map[string]interface{}{"message": "Not valid token"}
+	}
 }
