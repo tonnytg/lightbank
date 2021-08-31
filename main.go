d5bcdaf274de319188270a6a747822b303bd3af5
Antonio Thomacelli <tonnytg@gmail.com>
2021-08-31 19:37:49 -0300

    feat: update with database connector

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "github.com/tonnytg/lightbank/api"
+import (
+	"github.com/tonnytg/lightbank/api"
+	"github.com/tonnytg/lightbank/database"
+)
 
 func main() {
-	// api migration
+
+	// To prepare database with user and accounts, uncomment the next line
+	// migrations.Migrate()
+
+	// To prepare database for transactions, uncomment the next line
+	// migrations.MigrateTransaction()
+
+	// Start database Pool
+	database.InitDatabase()
+
+	// To start API Server
 	api.StartApi()
 }
