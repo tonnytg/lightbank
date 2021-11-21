9907d9c06eb77f755d14ea917fe3d19684128551
Antonio Thomacelli <tonnytg@gmail.com>
2021-11-21 15:28:08 -0300

    doc: update reademe

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "github.com/tonnytg/lightbank/api"
+import (
+	"github.com/tonnytg/lightbank/api"
+	"github.com/tonnytg/lightbank/migrations"
+	"os"
+)
 
 func main() {
-	// api migration
-	api.StartApi()
+
+	if len(os.Args) > 1 {
+		// Create Database
+		migrations.Migrate()
+	} else {
+		// api migration
+		api.StartApi()
+	}
 }
