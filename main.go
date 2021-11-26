daf557596d5a0f923c14d908af0d05c88631064f
Antonio Thomacelli <tonnytg@gmail.com>
2021-11-25 22:50:02 -0300

    feat: add global database conf

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
- package main
+package main
 
 import (
 	"github.com/tonnytg/lightbank/api"
@@ -8,7 +8,17 @@ import (
 )
 
 func init() {
-	database.InitDatabase()
+
+	dbconf := database.DBConfg{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		Name:     os.Getenv("DB_NAME"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+	}
+
+	database.InitDatabase(&dbconf)
 }
 
 func main() {
