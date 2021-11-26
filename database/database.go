c36b20ea0a6300917eef1f696d9ef34a9b9ea031
Antonio Thomacelli <tonnytg@gmail.com>
2021-11-25 22:47:48 -0300

    feat: add env vars

--- database/database.go
+++ database/database.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"fmt"
 	"github.com/tonnytg/lightbank/helpers"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -8,8 +9,18 @@ import (
 
 var DB *gorm.DB
 
-func InitDatabase() {
-	dsn := "host=127.0.0.1 user=postgres password=postgres dbname=lightbank port=5432 sslmode=disable"
+type DBConfg struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Name     string
+	SSLMode  string
+}
+
+func InitDatabase(cf *DBConfg) {
+	dsn := fmt.Sprintf("host=%s port=%s sslmode=%s user=%s password=%s dbname=%s",
+		cf.Host, cf.Port, cf.SSLMode, cf.User, cf.Password, cf.Name)
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	helpers.HandleErr(err)
 	DB = database
