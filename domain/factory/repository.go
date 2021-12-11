5ce15021bc1e3b8aef0db64e94a70ee718ed942e
Antonio Thomacelli <tonnytg@gmail.com>
2021-12-10 22:23:11 -0300

    feat: add respository transaction

--- domain/factory/repository.go
+++ domain/factory/repository.go
@@ -1,1 +1,9 @@
 package factory
+
+import (
+	"github.com/tonnytg/lightbank/domain/repository"
+)
+
+type RepositoryFactory interface {
+	CreateTransactionRepository() repository.TransactionRepository
+}
--- domain/repository/repository.go
+++ domain/repository/repository.go
@@ -1,6 +1,5 @@
 package repository
 
 type TransactionRepository interface {
-	Insert(id string, account string, amount string, status string, errorMessage string) error
+	Insert(id string, account string, amount float64, status string, errorMessage string) error
 }
-
